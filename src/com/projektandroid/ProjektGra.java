a08c40813536c6802c8beb1d6df53119ed9931ed
rpietrzko <r.pietrzko@gmail.com>
2012-11-30 20:20:41 +0100

    Projekt Androidowy

---
+++ src/com/projektandroid/ProjektGra.java
@@ -0,0 +1,34 @@
+package com.projektandroid;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.Menu;
+//import android.view.View;
+//import android.widget.Button;
+//import android.view.View.OnClickListener;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class ProjektGra extends AndroidApplication {
+
+	//private Button button;
+	
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.activity_projekt_gra);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        
+        
+        final AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useGL20 = false;
+        cfg.useAccelerometer = true;
+        initialize(new Gra(), cfg);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_projekt_gra, menu);
+        return true;
+    }
+}
